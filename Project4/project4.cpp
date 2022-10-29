39a4419ade83c8e2fe5dc6793f3ca73b81b80812
khoicodesthings <khoitrinh@ou.edu>
2022-10-29 13:17:22 -0500

    Add project files.

---
+++ Project4/project4.cpp
@@ -0,0 +1,54 @@
+// Project 4
+// Fall 2022
+// Computing Structures
+
+#include <iostream>
+#include <list>     // stl list library
+#include <iterator> // to iterate through the list
+using namespace std;
+
+class GL; // prototype for class GL
+// node class to store char and generalized linked list called down
+class node
+{
+protected:
+	char* charVariable; // single char variable pointer (not an array)
+	GL* down;           // going down to the next level
+public:
+	node(); // default constructor
+	// setters
+	void setCharVariable(char var);
+	void setDown(GL* d);
+	// getters
+	char* getChar();
+	GL* getDown();
+	// display
+	void displayChar();
+};
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// class Generalized List
+class GL
+{
+protected:
+	list<node> head; // head of the list - each object is of type node class
+public:
+	GL();                                         // default constructor
+	void buildGL(string l);                       // to build the generalized list
+		bool findCharInExpression(char findThisChar); // given a char, find it in a given expression
+		int LCAdistance(char char1, char char2);      // find the LCA distance of two char in the expression
+		int heightOfGL();                             // return the height of the GL using the GL
+		void searchDuplicates();                      // print all the duplicates in the GL
+		void display();                               // display list
+};
+int main()
+{
+	//int numExpressions; // number of expressions
+	//char option;        // command from the input file
+	// read the number of expressions given
+	//cin >> numExpressions;
+	//cout << "Number of expressions: " << numExpressions << endl;
+	//GL* expressions = new GL[numExpressions]; // an array of expressions / an array of GL objects
+		// read one line/expression at a time and call the buildGL method for each expression[i]
+		// read in and process all the options using switch case
+	return 0;
+} // main
